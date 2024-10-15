4b477ead929107c2036c64d43e3c3cd6bbcb022e
Tomás Ferreyra <tomasferreyra62@gmail.com>
2024-10-15 17:38:34 -0300

    Puntos verdes agregados a la BD
    Puntos Verdes agregados a la BD.

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PuntoVerde
 
 # Register your models here.
+admin.site.register(PuntoVerde)
--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
 class Item(models.Model):
+    """
+    Modelo que representa un ítem genérico en el sistema.
+    """
     name = models.CharField(max_length=100)
     description = models.TextField()
+    def __str__(self):
+        return str(self.name)
 
-    def _str_(self):
-        return self.name 
+class PuntoVerde(models.Model):
+    """
+    Modelo que representa un Punto Verde en el sistema.
+    Almacena información sobre ubicaciones de reciclaje y sus características.
+    """
+    id = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    materiales = models.TextField()
+    mas_info = models.TextField()
+    dia_hora = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    cooperativa = models.CharField(max_length=255, blank=True)
+    calle = models.CharField(max_length=100)
+    altura = models.IntegerField(null=True, blank=True)
+    calle2 = models.CharField(max_length=100, blank=True)
+    barrio = models.CharField(max_length=50)
+    comuna = models.CharField(max_length=20)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    objects = models.Manager()
+    def __str__(self):
+        return str(self.nombre)
