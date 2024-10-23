727636151d3bc8b3a575f078edde6f08f044c939
Tomás Ferreyra <tomasferreyra62@gmail.com>
2024-10-23 17:46:09 -0300

    Consejos RR
    - Agregué al administrador de Django la funcionalidad para poder administrar los consejos desde ahí mismo.
    - Modifiqué algunos consejos repetidos y agregué nuevos.

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import PuntoVerde
+from .models import PuntoVerde, ConsejosRRR
 # Register your models here.
 admin.site.register(PuntoVerde)
+admin.site.register(ConsejosRRR)
