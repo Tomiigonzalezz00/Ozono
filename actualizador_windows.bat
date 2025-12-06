487a416471d7aaa71b37a6486bc123f8be9c7ddc
Juan Gallardo <juanigallardo2001@gmail.com>
2025-12-06 15:49:49 -0300

    update del workflow de n8n automatico

--- actualizador_windows.bat
+++ actualizador_windows.bat
@@ -12,6 +12,12 @@ echo 2. Reconstruyendo y levantando Docker...
 docker-compose up -d --build
 
 echo.
+echo 3. Actualizando workflow de n8n...
+timeout /t 15 /nobreak
+docker exec mi_n8n n8n import:workflow --input=/workflows/Ozono.json
+docker exec mi_n8n n8n update:workflow --id=uvTUAQ4zIKdJAHZS --active=true
+
+echo.
 echo ==========================================
 echo Se actualizo OZONO correctamente.
 echo Puedes cerrar esta ventana.
